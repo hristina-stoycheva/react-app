fb368bded441434f193ec64ef11d6316067c241f
Hristina Stoycheva <hristina.stoycheva@eightcap.com>
2024-11-07 13:50:23 +0200

    adding files

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import ListGroup from "./components/ListGroup";
+function App() {
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  items = [];
+
+  return (
+    <>
+      <h1>List</h1>
+      {items.length === 0 ? <p>No items found</p> : null}
+      {items.length===0 && <p>No items found</p>}
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+export default App;
