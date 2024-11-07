30c47ba1f91655d5db12112de02a6be4fcab7f8b
Hristina Stoycheva <hristina.stoycheva@eightcap.com>
2024-11-07 14:04:25 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-      <h1>List</h1>
+      <h1>Test</h1>
       {items.length===0 && <p>No items found</p>}
       <ul className="list-group">
         {items.map((item) => (
